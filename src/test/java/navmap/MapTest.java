7146a5b05d8fb40706bba9f8be3d9087338b7517
Beatrix Klebe <bea@klebe.blog>
2020-07-01 20:33:25 -0400

    Add test for Usage annotation

---
+++ src/test/java/navmap/MapTest.java
@@ -0,0 +1,31 @@
+package navmap;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MapTest {
+    private final PrintStream stdOut;
+
+    MapTest() {
+        this.stdOut = System.out;
+    }
+
+    @Test
+    void main() {
+        var testStdOut = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(testStdOut));
+
+        Map.main(new String[0]);
+
+        assertEquals("Usage: Graph data.txt [-show] [-directions startIntersection endIntersection] [-meridianmap]\n", testStdOut.toString());
+    }
+
+    @AfterEach
+    void tearDown() {
+        System.setOut(this.stdOut);
+    }
+}
