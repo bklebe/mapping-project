ba2162e5d3afd6e13cdde6ff3b8c1d5293ee8896
Beatrix Klebe <beatrixk@hey.com>
2020-12-02 01:33:34 -0500

    Spotless apply

--- src/main/java/navmap/Map.java
+++ src/main/java/navmap/Map.java
@@ -3,7 +3,8 @@ package navmap;
 final class Map {
   private Map() {}
 
-  public static void main(final String[] args) {var launcher = new Launcher(args, System.out, new FileLoader());
+  public static void main(final String[] args) {
+    var launcher = new Launcher(args, System.out, new FileLoader());
     launcher.launch();
   }
 }
